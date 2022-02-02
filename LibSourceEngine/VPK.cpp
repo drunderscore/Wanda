0999aa43c224b1674969c449e3225095c0d073f8
James Puleo <james@jame.xyz>
2022-02-02 02:39:24 -0500

    SourceEngine: Introduce `VPK` class to parse VPK files and fully read data
    Works for version 2 VPKs, version 1 VPKs are still in use by some games.
    In the future, this shouldn't just automatically load all of the data in
    the VPK archives -- they are often huge and will easily take all memory.

---
+++ LibSourceEngine/VPK.cpp
@@ -0,0 +1,136 @@
+#include <AK/Stream.h>
+#include <AK/Vector.h>
+#include <LibCore/File.h>
+#include <LibCore/FileStream.h>
+#include <LibSourceEngine/VPK.h>
+
+namespace SourceEngine
+{
+ErrorOr<VPK> VPK::try_parse_from_file_path(StringView path, Function<ErrorOr<ByteBuffer>(u16)> resolve_external_archive)
+{
+    auto vpk_file = TRY(Core::File::open(String::formatted("{}_dir.vpk", path), Core::OpenMode::ReadOnly));
+    Core::InputFileStream file_stream(vpk_file);
+
+    return try_parse(file_stream, move(resolve_external_archive));
+}
+
+ErrorOr<VPK> VPK::try_parse_from_file_path(StringView path)
+{
+    auto file = TRY(Core::File::open(String::formatted("{}_dir.vpk", path), Core::OpenMode::ReadOnly));
+    Core::InputFileStream file_stream(file);
+
+    return try_parse(file_stream, [&](auto archive_index) -> ErrorOr<ByteBuffer> {
+        return TRY(Core::File::open(String::formatted("{}_{:#03}.vpk", path, archive_index), Core::OpenMode::ReadOnly))
+            ->read_all();
+    });
+}
+
+ErrorOr<VPK> VPK::try_parse(InputStream& stream,
+                            Function<ErrorOr<ByteBuffer>(u16 archive_index)> resolve_external_archive)
+{
+    u32 signature;
+    stream >> signature;
+
+    if (signature != VPK::signature)
+        return Error::from_string_literal("Invalid VPK signature");
+
+    VPK vpk;
+
+    u32 version;
+
+    stream >> version;
+
+    if (version != 2)
+        return Error::from_string_literal("VPK version must be 2");
+
+    stream >> vpk.m_tree_size;
+    stream >> vpk.m_file_data_section_size;
+    stream >> vpk.m_archive_md5_section_size;
+    stream >> vpk.m_other_md5_section_size;
+    stream >> vpk.m_signature_section_size;
+
+    // Files are represented as a tree, as strings, like this
+    // > Extension
+    //     > Directory Path
+    //         > Name
+    // A tree end when an empty string is encountered
+
+    // FIXME: This C-style forward-declaration and duplicated read_string is kinda yucky, any way we can improve it?
+    String extension;
+    String directory_path;
+    String name;
+
+    extension = read_string(stream);
+
+    while (!extension.is_empty())
+    {
+        directory_path = read_string(stream);
+
+        while (!directory_path.is_empty())
+        {
+            name = read_string(stream);
+
+            while (!name.is_empty())
+            {
+                Entry entry;
+
+                stream >> entry.m_crc;
+                stream >> entry.m_preload_bytes;
+                stream >> entry.m_archive_index;
+                stream >> entry.m_entry_offset;
+                stream >> entry.m_entry_length;
+
+                // TODO: proper error obviously
+                if (entry.m_archive_index == 0x7fff)
+                    return Error::from_string_literal("something is wrong with our archive index");
+
+                // TODO: proper error obviously
+                if (entry.m_entry_offset == 0x7fff)
+                    return Error::from_string_literal("something is wrong with our entry offset");
+
+                u16 terminator;
+                stream >> terminator;
+
+                // TODO: This might not be 0xFFFF if there is preload data right after, which we can check for...
+                if (terminator != 0xFFFF)
+                    return Error::from_string_literal("Expected 0xFFFF for a terminator");
+
+                if (!vpk.m_archives.contains(entry.m_archive_index))
+                    vpk.m_archives.set(entry.m_archive_index, TRY(resolve_external_archive(entry.m_archive_index)));
+
+                auto& archive_bytes = vpk.m_archives.find(entry.m_archive_index)->value;
+
+                entry.m_data = archive_bytes.bytes().slice(entry.m_entry_offset, entry.m_entry_length);
+
+                vpk.m_entries.set(String::formatted("{}/{}.{}", directory_path, name, extension), move(entry));
+
+                name = read_string(stream);
+            }
+
+            directory_path = read_string(stream);
+        }
+
+        extension = read_string(stream);
+    }
+
+    return vpk;
+}
+
+// FIXME: This doesn't really belong here :^(
+String VPK::read_string(InputStream& stream)
+{
+    Vector<char> characters;
+
+    char c;
+
+    stream >> c;
+
+    while (c != '\0')
+    {
+        characters.append(c);
+        stream >> c;
+    }
+
+    return {characters.data(), characters.size()};
+}
+}
---
+++ LibSourceEngine/VPK.h
@@ -0,0 +1,65 @@
+#pragma once
+
+#include <AK/Error.h>
+#include <AK/Function.h>
+#include <AK/StdLibExtraDetails.h>
+#include <AK/String.h>
+#include <AK/Types.h>
+
+namespace SourceEngine
+{
+
+// There are two versions of the VPK format still in use. The very original VPK format does not have a signature, and
+// has not been used since 2009, so we do not support it.
+
+// This implementation covers version 2 of VPK, as it is used by games I am more interested in supporting
+// (TF2 and CS:GO, plus many others). We should sill have a version 1 implementation in the future, as it's used by
+// games that are worthwhile to support too (L4D2, SFM)
+// Much of this implementation is based on https://developer.valvesoftware.com/wiki/VPK_File_Format
+// TODO: Support Version 1, for games like L4D2 and SFM.
+// TODO: Check CRC of entries
+// TODO: What is preload_bytes inside of VPK::Entry? What is preload?
+class VPK
+{
+public:
+    class Entry
+    {
+    public:
+        friend VPK;
+
+        u32 crc() const { return m_crc; }
+        Bytes data() const { return m_data; }
+
+    private:
+        u32 m_crc{};
+        u16 m_preload_bytes{};
+        u16 m_archive_index{};
+        u32 m_entry_offset{};
+        u32 m_entry_length{};
+        Bytes m_data{};
+    };
+
+    static ErrorOr<VPK> try_parse_from_file_path(
+        StringView path, Function<ErrorOr<ByteBuffer>(u16 archive_index)> resolve_external_archive);
+    static ErrorOr<VPK> try_parse_from_file_path(StringView path);
+
+    static ErrorOr<VPK> try_parse(InputStream&,
+                                  Function<ErrorOr<ByteBuffer>(u16 archive_index)> resolve_external_archive);
+
+    const HashMap<String, Entry>& entries() const { return m_entries; }
+
+private:
+    static constexpr u32 signature = 0x55AA1234;
+
+    u32 m_tree_size{};
+    u32 m_file_data_section_size{};
+    u32 m_archive_md5_section_size{};
+    u32 m_other_md5_section_size{};
+    u32 m_signature_section_size{};
+
+    HashMap<String, Entry> m_entries;
+    HashMap<u16, ByteBuffer> m_archives;
+
+    static String read_string(InputStream&);
+};
+}
